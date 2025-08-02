dcde6f945cbf74c3677e01a9d9ccadff4680339a
17Joako <joakopacomorales@gmail.com>
2025-08-02 00:23:09 +0000

    B: main.py changed, now reads and prints the books

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+    
+def main ():
+    path = "books/frankenstein.txt"
+    book_text = get_book_text(path)
+    print(book_text)
+
+main()
